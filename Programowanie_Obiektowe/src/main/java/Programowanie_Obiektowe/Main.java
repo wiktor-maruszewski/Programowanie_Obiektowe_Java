7998ab5ed46cc8bdd025b5739776a797554e0938
wiktor-maruszewski <wiktor.maruszewski@student.uwm.edu.pl>
2021-10-13 03:25:40 +0200

    commit

---
+++ Programowanie_Obiektowe/src/main/java/Programowanie_Obiektowe/Main.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Programowanie_Obiektowe;
+
+/**
+ *
+ * @author Wiktor
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //1
+        System.out.println(31 + 29 + 31);
+
+        //2
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+
+        //3
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+
+        //4
+        System.out.println(1000 * 1.06 * 1.06 * 1.06);
+
+        //5
+        System.out.println("+----+");
+        System.out.println("|Java|");
+        System.out.println("+----+\n");
+
+        //6
+        System.out.println("  /////  ");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("(| \u0e4f \u0e4f |)");
+        System.out.println("  | ^ |  ");
+        System.out.println(" | ,_, | ");
+        System.out.println(" +-----+ \n");
+
+        //10
+        System.out.println("Avatar: The Last Airbender");
+        System.out.println("Harry Potter");
+        System.out.println("Lord of The Rings\n");
+
+        //11
+        System.out.println("Nic dwa razy siê nie zdarza\n" +
+                "i nie zdarzy. Z tej przyczyny\n" +
+                "zrodziliœmy siê bez wprawy\n" +
+                "i pomrzemy bez rutyny.\n" +
+                "Choæbyœmy uczniami byli\n" +
+                "najtêpszymi w szkole œwiata,\n" +
+                "nie bêdziemy repetowaæ\n" +
+                "¿adnej zimy ani lata.\n" +
+                "¯aden dzieñ siê nie powtórzy,\n" +
+                "nie ma dwóch podobnych nocy,\n" +
+                "dwóch tych samych poca³unków,\n" +
+                "dwóch jednakich spojrzeñ w oczy.\n" +
+                "\n" +
+                "Wczoraj, kiedy twoje imiê\n" +
+                "ktoœ wymówi³ przy mnie g³oœno,\n" +
+                "tak mi by³o jakby ró¿a\n" +
+                "przez otwarte wpad³a okno.\n" +
+                "Dziœ, kiedy jesteœmy razem,\n" +
+                "odwróci³am twarz ku œcianie.\n" +
+                "Ró¿a? Jak wygl¹da ró¿a?\n" +
+                "Czy to kwiat? A mo¿e kamieñ?\n" +
+                "\n" +
+                "Czemu ty siê, z³a godzino,\n" +
+                "z niepotrzebnym mieszasz lêkiem?\n" +
+                "Jesteœ - a wiêc musisz min¹æ.\n" +
+                "Miniesz - a wiêc to jest piêkne.\n" +
+                "Uœmiechniêci, wspó³objêci\n" +
+                "spróbujemy szukaæ zgody,\n" +
+                "choæ ró¿nimy siê od siebie\n" +
+                "jak dwie krople czystej wody.");
+    }
+    
+}
+
