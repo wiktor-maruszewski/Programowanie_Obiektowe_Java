6757d26adccadf93337574da90348d33a8a6eb1a
wiktor-maruszewski <wiktor.maruszewski@student.uwm.edu.pl>
2022-02-01 15:14:50 +0100

    commit

---
+++ EGZAMIN_WIKTOR_PO/src/E01_162690/Z3/Zad3.java
@@ -0,0 +1,15 @@
+package E01_162690.Z3;
+
+import java.io.File;
+
+public class Zad3 {
+
+    public static void sortuj(File[] a){
+        for(int i = 0; i < a.length; i++){
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
